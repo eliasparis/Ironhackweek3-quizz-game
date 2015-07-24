23b2a9b06723ebd02f341708ca5fc26b3bd12882
Eli <55drk1@gmail.com>
2015-07-24 17:53:21 +0200

    third iteration bonus question

--- listofdef.js
+++ listofdef.js
@@ -2,6 +2,7 @@
 var prompt = require('prompt');
 
 
+//----- formatin questions
 var Question = function  (text,answer,id) 
 {
 	this.text = text;
@@ -10,63 +11,113 @@ var Question = function  (text,answer,id)
 	this.points = 3;
 }
 
+var Randomnumber = function (length)
+{
+
+}
+
 //------ creating questions
-var question1 = new Question('¿Capital de España?','madrid',0001);
-var question2 = new Question('¿Capital de Francia?','paris',0002);
-var question3 = new Question('¿Capital de Alemania?', 'berlin',0003);
+var question1 = new Question('¿Capital de España?','madrid',1);
+var question2 = new Question('¿Capital de Francia?','paris',2);
+var question3 = new Question('¿Capital de Alemania?', 'berlin',3);
+var question4 = new Question('¿Capital de Portugal?', 'lisboa',4);
 //------
 
 
 var Quizz = function ()
 {
+	
+	this.listOfQuestions = [];	
 	var totalpoints = 0;
 	var i = 0;
-	this.listOfQuestions = [];	
+	//------ set a random number dependig on the number of cuestions
+	this.randomnumber = Math.floor(Math.random() * this.listOfQuestions.length +1);
+	//------
+	
 	
 	this.play = function ()
 	{	
-			quizz.makequestion();
-			
+		quizz.makequestion();
 	};
 	
+
 	this.useprompt = function ()
 	{
+		//console.log(randomnumber);
 		prompt.start();
 				prompt.get(['answer'], function (err, result)
 				{
-					if (result.answer.toLowerCase() === quizz.listOfQuestions[i]['answer']) 
-					{
-						totalpoints += quizz.listOfQuestions[i]['points'];
-						console.log('Well done, you have ' + totalpoints + ' points. \n ');
-						i++;
+					if (result.answer.toLowerCase() === quizz.listOfQuestions[i]['answer'] && quizz.ifrandomnumber())
+					{ 
+						quizz.ifAnswerCorrectAndBonus();
 					
+					}else if (result.answer.toLowerCase() === quizz.listOfQuestions[i]['answer']) 
+					{
+						quizz.ifAnswerCorrect();
+
 					}else{
-						console.log('Wrong answer, you missed one point \n');
-						totalpoints -= 1 ;
-						
+						quizz.ifAnswerWrong();
 					}
 					quizz.makequestion();
 				});
 	}
 
+
+	//------ answer checker, based on the answer acts in different ways
+	this.ifAnswerCorrect = function ()
+	{
+		totalpoints += quizz.listOfQuestions[i]['points'];
+		console.log('Well done, you have ' + totalpoints + ' points. \n ');
+		i++;
+	}
+
+	this.ifAnswerCorrectAndBonus = function ()
+	{
+		totalpoints += quizz.listOfQuestions[i]['points']*2;
+		console.log('Well done, you have ' + totalpoints + ' points. \n ');
+		i++;
+	}
+
+	this.ifAnswerWrong = function ()
+	{
+		console.log('Wrong answer, you missed one point \n');
+		totalpoints -= 1 ;
+	}
+	//------ 
+
+	// this.calculatenum = function()
+	// {
+	// 	return randomnumber = Math.floor(Math.random() * this.listOfQuestions.length +1);
+	// }
+
+	this.ifrandomnumber = function ()
+	{
+		return quizz.listOfQuestions[i]['id'] === this.randomnumber;
+	}
+
 	this.makequestion = function()
 	{
-		
-		if (i < this.listOfQuestions.length) {
-			
+
+		if (i < this.listOfQuestions.length && quizz.ifrandomnumber() ) {
+			console.log('NEXT IS A BONUS QUESTION \nwhorts double points\n')
 			console.log(this.listOfQuestions[i]['text']);
 			quizz.useprompt();
 			
-		}else
-			console.log('Winner¡¡¡¡');
+		}else if (i < this.listOfQuestions.length) {
 			
-		;
+			console.log(this.listOfQuestions[i]['text']);
+			quizz.useprompt();
+		
+		}else{
+			console.log('Winner¡¡¡¡');
+		}				
 	}
 }
 
 Quizz.prototype.addquestion = function (question)
 {
 	this.listOfQuestions.push(question);
+	this.randomnumber = Math.floor(Math.random() * this.listOfQuestions.length +1);
 }
 
 
@@ -77,9 +128,19 @@ var quizz = new Quizz();
 quizz.addquestion(question1);
 quizz.addquestion(question2);
 quizz.addquestion(question3);
+quizz.addquestion(question4);
 
-//----- playing
+
+//----- playing the game
 quizz.play();
+
+
+
+
+
+
+
+
 
 
 
